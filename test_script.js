a3613d1514508ba5b62c2bb6841f634772463bb1
Keishon Ross <keishonross97@gmail.com>
2018-11-15 20:02:02 +0000

    takes first cli param after command and logs value to console

--- test_script.js
+++ test_script.js
@@ -1,6 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
-
+const input = process.argv[2];
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
@@ -20,7 +20,9 @@ client.connect((err) => {
       return console.error("error running query", err);
     }
     console.log("Connected")
-    console.log(result.rows[0].number); //output: 1
+
+    console.log(input);
+
     client.end();
     console.log("Disconnected")
   });
